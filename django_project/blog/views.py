b7b81d5f18d385ba60b6e2ebdf5a00a2b401c74e
Eduardo Imery <ejwimery@gmail.com>
2019-10-03 17:07:06 -0600

    forating the date file to show more organized

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,32 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
-posts = [
-    {
-        'author':'edward',
-        'title':'Blog Post 1',
-        'content':'Frist post content',
-        'date_posted':'December 1, 2019'
-    },
-    {
-        'author':'Oscar Carillo',
-        'title':'Blog Post 2',
-        'content':'Second post content',
-        'date_posted':'December 3, 2019'
-    },
-    {
-        'author':'Samantha Ricardo',
-        'title':'Blog Post 3',
-        'content':'Third post content',
-        'date_posted':'December 5, 2019'
-    },
-    {
-        'author':'Lucero Martinez',
-        'title':'Blog Post 4',
-        'content':'Frourt post content',
-        'date_posted':'December 8, 2019'
-    }
-]
 
 def home(request):
     context = {
