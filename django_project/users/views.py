8f355004adb7cffe660fb34b64d066a5c43abd14
Eduardo Imery <ejwimery@gmail.com>
2019-10-03 18:18:17 -0600

    creating the auth app with some validation form

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+    else:
+        from = UserCreationForm()
     return render(request, 'users/register.html', {'form':form})
